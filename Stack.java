29a7e1871777921c8a88bdb383db1f9a4a8f42eb
CLYDE <bazileclyde@gmail.com>
2017-02-26 16:21:37 -0500

    HEADER

--- Stack.java
+++ Stack.java
@@ -1,2 +1,12 @@
-//TODO
+import java.util.LinkedList;
+
+/** 
+* The Stack class represents a 
+* last-in-first-out (LIFO) stack of objects.
+*
+* @author 	Clyde-Bazile
+* @since 	2017-02-26
+*/
+
+
 
