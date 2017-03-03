2cff54983525dfac2b51bb2d1ba4d78f29011688
CLYDE <bazileclyde@gmail.com>
2017-03-03 18:00:51 -0500

    Allows Stack class to use newly created LinkedList class

--- Stack.java
+++ Stack.java
@@ -1,27 +1,27 @@
-import java.util.LinkedList;
-
 /** 
   * The Stack class represents a 
   * last-in-first-out (LIFO) stack of objects.
   *
-  * @author 	Clyde-Bazile
-  * @since 	2017-02-26
+  * @author Clyde-Bazile
+  * @since	2017-02-26
   */
-public class Stack<E>
+public final class Stack<E>
 {
 	private LinkedList<E> stack;
 
 	/** Creates an empty stack. */
-	public Stack()
-	{ stack = new LinkedList<>(); }
+	public Stack() { 
+		stack = new LinkedList<>();
+	}
 
 	/**
 	  * Pushes an item onto the top of this stack.
 	  *
 	  * @param item 	the item to be pushed onto this stack
 	  */
-	public void push(E item)
-	{ stack.addFirst(item); }
+	public void push(E item) { 
+		stack.append(item);
+	}
 
 	/** 
 	  * Removes the object at the top of this stack and returns
@@ -29,8 +29,9 @@ public class Stack<E>
 	  * 
 	  * @return the object at the top of this stack
 	  */
-	public E pop()
-	{ return stack.poll(); }
+	public E pop() { 
+		return stack.deleteLast(); 
+	}
 
 	/**
 	  * Looks at the object at the top of this stack without removing
@@ -38,8 +39,9 @@ public class Stack<E>
 	  *
 	  * @return the object at the top of this stack
 	  */
-	public E peek()
-	{ return stack.peek(); }
+	public E peek() { 
+		return stack.getLast(); 
+	}
 
 	/**
 	  * Test if this stack is empty.
@@ -47,8 +49,37 @@ public class Stack<E>
 	  * @return true if and only if this stack contains no items;
 	  * false otherwise
 	  */
-	public boolean isEmpty()
-	{ return stack.isEmpty(); }
+	public boolean isEmpty() { 
+		return stack.isEmpty(); 
+	}
+
+	/**
+	  * Removes all of the elements from this stack.
+	  */
+	public void clear() {
+		stack.clear();
+	}
+
+	/**
+	  * Returns the number of elements in this stack.
+	  *
+	  * @return the number of elements in this stack.
+	  */
+	public int size() {
+		return stack.size();
+	}
+
+	/**
+	  * {@inheritDoc}
+	  */
+	@Override
+	public String toString() {
+		StringBuilder list = new StringBuilder();
+		for (E item : stack) {
+			list.append(String.format("[%s|", item));
+		}
+		return list.toString();
+	}
 }
 
 
