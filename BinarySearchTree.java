f5d7c81f223475c7779b57addf66451668cad5cf
Clyde Bazile <bazileclyde@gmail.com>
2017-03-21 23:19:05 -0400

    Add initial code

---
+++ BinarySearchTree.java
@@ -0,0 +1,61 @@
+import java.util.Collection;
+
+/**
+  * Binary Search Tree implementation that extends the abstract
+  * {@code BinaryTree} class.
+  *
+  * @author Clyde-Bazile
+  * @since 2017-03-21
+  */
+public final class BinarySearchTree extends BinaryTree {
+
+	public BinarySearchTree() {
+		super();
+	}
+
+	public BinarySearchTree() {
+		super(final Collection<? extends E> c);
+	}
+
+	/**
+	  * {@inheritDoc}
+	  */
+	public void insert(final E e) {
+		// TODO
+	}
+
+	/**
+	  * {@inheritDoc}
+	  */
+	public E delete(final E e) {
+		// TODO
+	}
+
+	/**
+	  * {@inheritDoc}
+	  */
+	public E minimum() {
+		// TODO
+	}
+
+	/**
+	  * {@inheritDoc}
+	  */
+	public E maximum() {
+		// TODO
+	}
+
+	/**
+	  * {@inheritDoc}
+	  */
+	public boolean contains(final E e) {
+		// TODO
+	}
+
+	/**
+	  * {@inheritDoc}
+	  */
+	protected Node<E> search(final E data) {
+		// TODO
+	}
+}
